29359d266877c9c3aab4c88c574340fd1343c109
Josh Deeden <jdeeden@gmail.com>
2013-07-07 17:53:38 -0700

    move require 'reform/rails' to initializer

---
+++ lib/generators/reform/form/templates/form.rb
@@ -0,0 +1,20 @@
+class Forms::<%= class_prefix %>Form < Reform::Form
+  include DSL
+<%- unless skip_activerecord? -%>
+  include Reform::Form::ActiveRecord
+<%- end -%>
+
+<%- properties.keys.each do |model|
+  names = properties[model]
+-%>
+<%- if names.length == 1 -%>
+  property :<%= names[0] %>, on: :<%= model %>
+  <%- else -%>
+  properties [<%= names.map {|name| ":#{name}"}.join(", ") %>], on: :<%= model %>
+<%- end -%>
+<% end %>
+<%- if has_model? -%>
+  model :<%= model %>, on: :<%= model %>
+<%- end -%>
+
+end
